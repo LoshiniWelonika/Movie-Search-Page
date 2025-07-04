545a2b02c160b7b5b4e3ce06aad2ae74368c9e7f
LoshiniWelonika <welonikakedapanawala@gmail.com>
2025-07-04 22:39:59 +0530

    Project started. Moviecard and Home pages halfway done

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,27 @@
+import MovieCard from "../components/MovieCard";
+import { useState } from "react";
+
+function Home() {
+    const [searchQuery, setSearchQuery] = useState("");
+
+    const movies = [
+        {id:1, title:"John Wick", released_date:"2018"},
+        {id:2, title:"Sinners", released_date:"2025"},
+        {id:3, title:"Friends", released_date:"1998"},
+    ]
+
+    const handleSearch = () => {};
+
+    return <div className="home">
+        <form onSubmit={handleSearch} className="search-form">
+            <input type="text" placeholder="Search for movies..." />
+            <button type="submit" className="search-btn">Search</button>
+        </form>
+
+        <div className="movies-grid">
+            {movies.map(movie => <MovieCard movie={movie} key={movie.id} /> )}
+        </div>
+    </div>
+}
+
+export default Home;
