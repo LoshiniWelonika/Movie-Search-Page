568a9beec113058aa98c5663f153ec98fb4aad07
LoshiniWelonika <welonikakedapanawala@gmail.com>
2025-07-28 10:32:18 +0530

    Current Progress

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -10,16 +10,27 @@ function Home() {
         {id:3, title:"Friends", released_date:"1998"},
     ]
 
-    const handleSearch = () => {};
+    const handleSearch = (e) => {
+        e.preventDefault();
+        alert(searchQuery);
+        setSearchQuery("");
+    };
 
     return <div className="home">
         <form onSubmit={handleSearch} className="search-form">
-            <input type="text" placeholder="Search for movies..." />
+            <input 
+                type="text" 
+                placeholder="Search for movies..." 
+                value={searchQuery}
+                onChange={ e => {setSearchQuery(e.target.value)}}
+            />
             <button type="submit" className="search-btn">Search</button>
         </form>
 
         <div className="movies-grid">
-            {movies.map(movie => <MovieCard movie={movie} key={movie.id} /> )}
+            {movies.map(movie => 
+            <MovieCard movie={movie} key={movie.id} /> 
+            )}
         </div>
     </div>
 }
